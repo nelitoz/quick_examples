9fed89336bf1391db8a408bfa6a212accb601ea7
dacasti2 <dacasti2@nelitoz.com>
2021-11-07 12:29:39 -0800

    fixing wheen interface has no description

--- data_formats/3 - XML data formats/get_interface_status.py
+++ data_formats/3 - XML data formats/get_interface_status.py
@@ -7,20 +7,24 @@ def open_xml(file):
         xml_data = xmltodict.parse(xml_example)
     return xml_data
 
-xml_data = open_xml("dc1_int_down.xml")
+xml_data = open_xml("dc2_int_down.xml")
 
 #in case the Row interface is equal to a list, meaning there are more than 1 interface
 if type(xml_data["nf:rpc-reply"]["nf:data"]["show"]["interface"]["status"]["up"]["__readonly__"]["TABLE_interface"]["ROW_interface"])==list:
     for i in xml_data["nf:rpc-reply"]["nf:data"]["show"]["interface"]["status"]["up"]["__readonly__"]["TABLE_interface"][
         "ROW_interface"]:
         if re.match(r"Ethernet", i["interface"]):
-            print (i["interface"]+","+ i["state"]+","+i["name"])
+            if "name" in i:
+                print (i["interface"]+","+ i["state"]+","+i["name"])
+            else:
+                print (i["interface"]+","+ i["state"]+","+"NONE")
 
 #in case the Row interface is equal to dict, meaning there is just 1 interface
 else:
     interface = xml_data["nf:rpc-reply"]["nf:data"]["show"]["interface"]["status"]["up"]["__readonly__"]["TABLE_interface"]["ROW_interface"]
     table_interface = xml_data["nf:rpc-reply"]["nf:data"]["show"]["interface"]["status"]["up"]["__readonly__"]["TABLE_interface"]
     if "Ethernet" in interface["interface"]:
-        print (interface["interface"], interface["state"], interface["name"])
-
-
+        if "name" in interface["name"]:
+            print (interface["interface"], interface["state"], interface["name"])
+        else: 
+            print (interface["interface"], interface["state"])
